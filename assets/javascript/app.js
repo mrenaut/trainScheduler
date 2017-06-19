2bd550b39162c30380e6d1a5b4cc82a8c45d3688
Melissa <melissarenaut@gmail.com>
2017-06-19 00:00:50 -0400

    adding to and returning data from firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,123 @@
-//
-//<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
-//<script>
-//  // Initialize Firebase
-//  // TODO: Replace with your project's customized code snippet
-//  var config = {
-//    apiKey: "<API_KEY>",
-//    authDomain: "<PROJECT_ID>.firebaseapp.com",
-//    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-//    storageBucket: "<BUCKET>.appspot.com",
-//    messagingSenderId: "<SENDER_ID>",
-//  };
-//  firebase.initializeApp(config);
-//</script>
+    // Document ready function 
+    $(document).ready(function () {
 
 
-//
-//<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase-app.js"></script>
-//<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase-auth.js"></script>
-//<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase-database.js"></script>
-//<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase-messaging.js"></script>
-//
-//<!-- Leave out Storage -->
-//<!-- <script src="https://www.gstatic.com/firebasejs/4.1.2/firebase-storage.js"></script> -->
-//
-//<script>
-//  var config = {
-//    // ...
-//  };
-//  firebase.initializeApp(config);
-//</script>
-//
-
-// Document ready function 
-$(document).ready(function() {
-
-    
-    //Global Variables
-    var trainName = "";
-    var destinationName = "";
-    var trainTime = "";
-    var frequency = "";
-    var minutesAway = "";
-    
-  
-    //add a table row when the user enters information then hits the submit button      
-	$(document).on("click", "#submitButton", function(event) {
-		event.preventDefault();
-		trainName = $("#trainInput").val().trim();
-		console.log("train name: " + trainName);
-        destinationName = $("#destinationInput").val().trim();
-        console.log("destination name: " + destinationName);
-        trainTime = $("#timeInput").val().trim();
-        console.log("train time: " + trainTime);
-        frequency = $("#frequencyInput").val().trim();
-		console.log("frequency: " + frequency);
-        $("#table").append ("<tr>" +            
-        "<td>" + trainName + "</td>" +
-        "<td>" + destinationName + "</td>" +
-        "<td>" + trainTime + "</td>" +
-        "<td>" + frequency + "</td>" +
-        "<td>" + minutesAway + "</td>" +
-        "</tr>")         
+        //  Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyC908vOZuLJtdZtAKj-TXGDELfwGghBhcQ",
+            authDomain: "trainscheduler-465b5.firebaseapp.com",
+            databaseURL: "https://trainscheduler-465b5.firebaseio.com",
+            projectId: "trainscheduler-465b5",
+            storageBucket: "trainscheduler-465b5.appspot.com",
+            messagingSenderId: "753403446140"
+        };
         
-        formClearing ();
-          
-	});
+        firebase.initializeApp(config);
+        
             
-
-               
-    function formClearing() {
         
-      $("#trainInput").val("");
-      $("#destinationInput").val("");
-      $("#timeInput").val("");
-      $("#frequencyInput").val("");
-    }
-    
-   
-//    $.ajax({
-//				url: queryURL,
-//				method: "GET"
-//			}).done(function(response) {
-    
-    
-    
-//end of document ready function    
-});
+        
+
+        //gets current time for minutes away function (military time)
+        new Date($.now());
+        var dt = new Date();
+        var time = dt.getHours() + ":" + dt.getMinutes();
+        console.log(time);
+
+        //gets current time AM/PM (converted current time)
+        var options = {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        };
+        var timeString = dt.toLocaleString('en-US', options);
+        console.log(timeString);
+
+
+        //sets variables to change in database
+       // var database = firebase.database();
+        
+        //Global Variables
+        var trainName = "";
+        var destinationName = "";
+        var trainTime = "";
+        var frequency = "";
+        var minutesAway = "";
+
+
+        //captures information from input sectoin when user hits the submit button      
+        $(document).on("click", "#submitButton", function (event) {
+            
+            trainName = $("#trainInput").val().trim();
+            destinationName = $("#destinationInput").val().trim();
+            trainTime = $("#timeInput").val().trim();
+            frequency = $("#frequencyInput").val().trim();
+
+//              $("#table").append("<tr>" +
+//                "<td>" + trainName + "</td>" +
+//                "<td>" + destinationName + "</td>" +
+//                "<td>" + trainTime + "</td>" +
+//                "<td>" + frequency + "</td>" +
+//                "<td>" + minutesAway + "</td>" +
+//                "</tr>")
+            
+            event.preventDefault();
+            //adds information to database
+              firebase.database().ref().push({
+                  trainName: trainName,
+                  destinationName: destinationName,
+                  trainTime: trainTime,
+                  frequency: frequency,
+                  dateAdded:firebase.database.ServerValue.TIMESTAMP
+        });
+            
+          
+            //calls clear form again after submit
+            formClearing();
+        });
+        
+          //gets information from database and displays in table
+            firebase.database().ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function(snapshot) {
+                $("#table").append("<tr><td>" + 
+                                   snapshot.val().trainName + "</td><td>" + snapshot.val().destinationName + "</td><td>" + snapshot.val().trainTime + "</td><td>" + snapshot.val().frequency+ "</td><td>" + "minutesAway" + 
+                                   "</td><tr>" )
+                              
+            })
+
+        //function to clear form again after submit           
+        function formClearing() {
+
+            $("#trainInput").val("");
+            $("#destinationInput").val("");
+            $("#timeInput").val("");
+            $("#frequencyInput").val("");
+        }
+        
+        
+        
+        
+        
+        //Firebase access section
+        //===============================================================================================================
+
+        
+            
+            var clickCounter = 0;
+
+            
+            
+            
+            
+            
+            
+       // ===============================================================================================================
+
+        //    $.ajax({
+        //				url: queryURL,
+        //				method: "GET"
+        //			}).done(function(response) {
+
+
+
+        //end of document ready function    
+    });
