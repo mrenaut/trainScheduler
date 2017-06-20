4a33591f8e36fb83e93b60b36801f95fbae21891
Melissa <melissarenaut@gmail.com>
2017-06-20 17:32:24 -0400

    final minutes away updated and working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,62 +39,70 @@
             //converts user's input time into minutes        
             var a = trainTime.split(':'); // split it at the colons
             var convertedtrainTime = (+a[0]) * 60 + (+a[1]);
-            console.log(nextArrival);
-            
+
+            //determines if train time entered is after current time and calculates minutes away
             if (convertedtrainTime >= currentTimeInMinutes) {
                 nextArrival = trainTime; 
                 minutesAway = convertedtrainTime - currentTimeInMinutes;
-
-                }
+            }
                     
-      
+            //determines if train time entered is before current time and calculates minutes away
             if (convertedtrainTime < currentTimeInMinutes) {
-                console.log("converted train time:" + convertedtrainTime);
-                console.log("converted current time:" + currentTimeInMinutes);
-                //get the difference in minutes, example 15min
+//==========================================================================================    
+                //get the difference in minutes
                 var nextArrivalCalc = currentTimeInMinutes - convertedtrainTime; 
                 //divide the difference by the amount of minutes in frequency and get remainder
                 var remainder = nextArrivalCalc%frequency;
-                //subtract remainder from frequency and that should equal minutes away
+                //subtracts remainder from frequency and to get minutes away
                 minutesAway = frequency - remainder;
-                    
-//==========================================================================================      
-                unconvertedNextArrival = currentTimeInMinutes + minutesAway;
-                console.log("unconverted next arrival in minutes: " + unconvertedNextArrival);
-                convertingNextArrival = unconvertedNextArrival / 60;
-                //take number in front of decimal and display in hours
-                //+ modulus
-                console.log("remainder after converting division: " + convertingNextArrival);
                 
-             
-              //  nextArrival = 
+//==========================================================================================                    
+                    
+                //adds minutes away and current time
+                unconvertedNextArrival = currentTimeInMinutes + minutesAway;
+                console.log(unconvertedNextArrival);
+                //divides minutes away combined with current time to determine next arrival time and break into hours
+                convertingNextArrival = unconvertedNextArrival / 60;
+                console.log(convertingNextArrival);
+                
+                //if convertingNextArrival is a whole number/integer take number and display in nextArrival hours section and add :00 to the minutes section                
+                if (Number.isInteger(convertingNextArrival)) {
+                    nextArrival = convertingNextArrival + ":00";
+                }                 
 
-              
-                } 
-//==========================================================================================    
+                //if convertNextArrival is a float, need to take number after decimal only and multiply by 60 to convert to minutes and display in minutes section of nextArrival
+                else{
+                    console.log("not a whole number")
+                    var decPart = "." + (convertingNextArrival+"").split(".")[1];
+                    var intPart = (convertingNextArrival+"").split(".")[0];
+                    console.log(intPart);
+                    console.log(decPart);
+                    decPartInMinutes = Math.round(decPart * 60);              
+                    console.log(decPartInMinutes);
+                    nextArrival = intPart + ":" + decPartInMinutes;
+                }
+                
+            } 
 
-               
-            
             //adds information to database
             firebase.database().ref().push({
                   trainName: trainName,
                   destinationName: destinationName,
                   nextArrival: nextArrival,
                   frequency: frequency,
+                  minutesAway: minutesAway,
                   dateAdded:firebase.database.ServerValue.TIMESTAMP
             });
                       
             //calls clear form again after submit
             formClearing();
             
-            
-
         });
         
           //gets information from database and displays in table
             firebase.database().ref().orderByChild("dateAdded").limitToLast(5).on("child_added", function(snapshot) {
                 $("#table").append("<tr><td>" + 
-                snapshot.val().trainName + "</td><td>" + snapshot.val().destinationName + "</td><td>" + snapshot.val().nextArrival + "</td><td>" + snapshot.val().frequency+ "</td><td>" + minutesAway + 
+                snapshot.val().trainName + "</td><td>" + snapshot.val().destinationName + "</td><td>" + snapshot.val().nextArrival + "</td><td>" + snapshot.val().frequency + "</td><td>" + snapshot.val().minutesAway + 
                 "</td><tr>" )
                 })
 
@@ -106,7 +114,6 @@
             $("#frequencyInput").val("");
             }
         
-
    
  //if trainTime is ealier than now, subtract difference to find out how many minutes have passed since then
 //if trainTime is later than now, calculate how many minutes are betweeen now and then
@@ -115,10 +122,7 @@
 //subtract modulus from frequency
 //add difference to current time and update that info into variable   
         
-        
-//gets current time
-//=================================================================================================================
-        
+      
         //gets current time for minutes away function (military time)
         new Date($.now());
         var dt = new Date();
@@ -129,10 +133,6 @@
         var currentTimeInMinutes= (dt.getHours() * 60) + dt.getMinutes();
         console.log(currentTimeInMinutes);
         
-        
-
-        
-     
         
         //end of document ready function    
     });
